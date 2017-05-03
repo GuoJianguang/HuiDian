8b58c7136f5b2b7316639129472d21beb6a6b1a5
GuoJianguang <mac@macdeMacBook-Pro.local>
2017-05-03 16:04:56 +0800

    Initial Commit

---
+++ HuiDian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuiDian
+//
+//  Created by mac on 2017/5/3.
+//  Copyright © 2017年 Huidian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HuiDian/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HuiDian
+//
+//  Created by mac on 2017/5/3.
+//  Copyright © 2017年 Huidian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
